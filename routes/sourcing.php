1f1107ed39a843d5670143c32c4b6a092791d175
Mostafa-04 <mostafaalmannani@gmail.com>
2026-05-06 15:18:00 +0100

    feat(sourcing): connect Inertia route to Sourcing Index page

--- routes/sourcing.php
+++ routes/sourcing.php
@@ -2,7 +2,6 @@
 
 use Inertia\Inertia;
 
-Route::get('/sourcing', fn () => Inertia::render('ComingSoon', [
-    'title' => 'Sourcing automatique',
-    'description' => 'Le sourcing automatique sera disponible prochainement.',
-]))->name('sourcing');
+Route::get('/sourcing', function () {
+    return Inertia::render('Sourcing/Index');
+})->name('sourcing');
